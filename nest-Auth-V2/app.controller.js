2467b04ce43d7f7d4f4b42f6f86be6a0708e8c17
Swapnil <swapniladsul017@gmail.com>
2021-07-02 17:53:26 +0530

    Add files via upload

---
+++ nest-Auth-V2/app.controller.js
@@ -0,0 +1,64 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppController = void 0;
+const common_1 = require("@nestjs/common");
+const app_service_1 = require("./app.service");
+const authenticated_session_guard_1 = require("./auth/authenticated.session.guard");
+const local_auth_guard_1 = require("./auth/local-auth.guard");
+let AppController = class AppController {
+    constructor(appService) {
+        this.appService = appService;
+    }
+    login(req) {
+        console.log("Finally Logged In");
+        return { msg: "Logged In Successfully" };
+    }
+    getHello(req) {
+        return req.user;
+    }
+    logout(req) {
+        req.logout();
+        return { msg: "Logged Out Successfully" };
+    }
+};
+__decorate([
+    common_1.UseGuards(local_auth_guard_1.LocalAuthGuard),
+    common_1.Post('login'),
+    __param(0, common_1.Request()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Object)
+], AppController.prototype, "login", null);
+__decorate([
+    common_1.UseGuards(authenticated_session_guard_1.AuthenticatedGuardSession),
+    common_1.Get('protected'),
+    __param(0, common_1.Request()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", String)
+], AppController.prototype, "getHello", null);
+__decorate([
+    common_1.Get('logout'),
+    __param(0, common_1.Request()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Object)
+], AppController.prototype, "logout", null);
+AppController = __decorate([
+    common_1.Controller(),
+    __metadata("design:paramtypes", [app_service_1.AppService])
+], AppController);
+exports.AppController = AppController;
+//# sourceMappingURL=app.controller.js.map
